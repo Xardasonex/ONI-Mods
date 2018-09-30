aef8a7eb52c1279c7a1b041cb944681d50625868
Sijko <32700053+Sijko@users.noreply.github.com>
2018-09-30 17:05:33 +0300

    Add files via upload

---
+++ src/SuperAlgaeDist/SADState.cs
@@ -0,0 +1,12 @@
+﻿using ONI_Common.Json;
+
+namespace SADState
+{
+    class SADState
+    {
+        public float SlimeConsumeKGsPerSecond { get; set; } = 0.6f;
+
+        public static BaseStateManager<SADState> StateManager
+            = new BaseStateManager<SADState>("SAD");
+    }
+}
