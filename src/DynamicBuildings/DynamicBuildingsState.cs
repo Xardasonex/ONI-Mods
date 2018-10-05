347abfe6f0037ca57490d9e1e6b8b1b9851234d6
Sijko <32700053+Sijko@users.noreply.github.com>
2018-10-05 20:28:21 +0300

    up

--- src/DynamicBuildings/DynamicBuildingsState.cs
+++ src/DynamicBuildings/DynamicBuildingsState.cs
@@ -3,7 +3,8 @@
     class DynamicBuildingsState
 {
         public float liquidandgas { get; set; } = 20;
-        public float STA { get; set; } = 1000;
+    public float Solid { get; set; } = 20;
+    public float STA { get; set; } = 1000;
         public float SlimeConsumeKGsPerSecond { get; set; } = 1f;
         public byte SuckInRadius { get; set; } = 2;
 
