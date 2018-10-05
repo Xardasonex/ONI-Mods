678c75a8456d1a706615c8059d3c8622e92e9eb1
Sijko <32700053+Sijko@users.noreply.github.com>
2018-10-05 20:29:37 +0300

    new

---
+++ src/Stamina/Stamina.cs
@@ -0,0 +1,19 @@
+﻿using Harmony;
+using System;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using System.Linq;
+
+namespace KorribanDynamics
+{
+    [HarmonyPatch(typeof(MinionConfig))]
+    [HarmonyPatch("CreatePrefab")]
+    public static class StaminaPatch
+    {
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var codes = new List<CodeInstruction>(instructions);
+            for (int i = 0; i < codes.Count; i++)
+            {if ((codes[i].opcode == OpCodes.Ldc_R4) && ((Single)codes[i].operand == -0.116666667f)){{codes[i].operand = 0f;}}}
+            return codes.AsEnumerable();}}}
+ 
